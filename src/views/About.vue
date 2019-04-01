abf18d1d3daff30d22cb877b15cf543df286e63a
XUELANYING <18735122505@163.com>
2019-04-01 11:54:52 +0800

    home页面动态路由

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+      <p v-for="val in resouce">{{val}}</p>
   </div>
 </template>
+<script>
+    import axios from "axios";
+
+
+    export default {
+        name: 'home',
+        created() {
+            axios.get("/api/web/api/floors/v1?labe")
+                .then((data) => {
+                    console.log(data)
+                    this.resouce = data.data.data.container.floor;
+                })
+        },
+        components: {},
+        data() {
+            return {
+                resouce: []
+            }
+        }
+    }
+
+</script>
